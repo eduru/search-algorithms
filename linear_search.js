865cd6a2d1dda29d2732dccf4f5cd1d03af100c7
Edur <eduru@me.com>
2021-03-01 13:48:00 +0900

    linearSearch completed

---
+++ linear_search.js
@@ -0,0 +1,25 @@
+// indexOf, includes,find, findIndex (they're using linear search)
+
+// Time Complexity:
+// BEST CASE = > O(1)
+// WORST CASE,AVERAGE => O(n)
+// Space Complexity: O(1)
+
+// This function accepts an array and a value.
+// Loop through the array and check if the current array element is equal to the value.
+// If it is, return the index at wich the element is found.
+// If the value is never found, return -1
+
+const linearSearch = (arr, value) => {
+  const len = arr.length;
+  for (let i = 0; i < len; i++) {
+    if (arr[i] === value) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+const array = ["a", "b", "c", "d", "e", "f", "g", "h", "i"];
+
+console.log(linearSearch(array, "e"));
